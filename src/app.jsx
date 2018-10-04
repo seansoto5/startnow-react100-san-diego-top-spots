b5970bfdf47c22b7d51c4928765ceb14d4ffa0d8
seansoto5 <sean_soto125@yahoo.com>
2018-10-03 21:12:37 -0700

    Finished and working project

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import TopSpot from './topspot';
+import topspot from './topspot';
 
 class App extends Component {
   constructor(props) {
@@ -20,30 +21,26 @@ class App extends Component {
 
   render() {
     return (
-      <div className='App'>
-        <div className='container' style={{marginTop:'60px'}}>
+      <div className='App' style={{backgroundColor:'blue'}}>
+        <div className='container' style={{margin:'auto'}}>
           <div className='row' style={{marginBottom:'30px'}}>
             <div className='column'>
-              <div name='title section' style={{border:'1px solid black'}}>
-                <h1 style={{margin:'30px 30px 10px 30px'}}>San Diego Top Spots</h1>
+              <header name='title-section' style={{border:'1px solid black', backgroundColor:'white'}}>
+                <h1 style={{margin: '30px 30px 10px 30px'}}>San Diego Top Spots</h1>
                 <h5 style={{margin:'10px 30px 30px 30px'}}>A list of the top 30 places to see in San Diego, CA</h5>
-              </div>
+              </header>
             </div>
           </div>
           <div className='row'>
             <div className='column'>
-              <div className='data-section'>
-                {/* {
-                  this.state.topspots.map(topspots => (
-                    <TopSpot
-                      key={topspot.id}
-                      name={topspot.name}
-                      description={topspot.description}
-                      location={topspot.location}/>
-                  ))
-                } */}
-                <pre>{JSON.stringify(this.state.topspots, null, 2)}</pre>
-              </div>
+              {/* <pre>{JSON.stringify(this.state.topspots, null, 2)}</pre> */}
+              {this.state.topspots.map(topspots => (
+                <TopSpot
+                  key={topspots.id}
+                  name={topspots.name}
+                  description={topspots.description}
+                  location={topspots.location} />
+              ))}
             </div>
           </div>
         </div>
--- src/topspot.jsx
+++ src/topspot.jsx
@@ -4,5 +4,6 @@ export default props => (
     <div className='well'>
         <h4>{props.name}</h4>
         <p>{props.description}</p>
+        <a href={'https://maps.google.com/?q=' + props.location[0] + ',' + props.location[1]}>Open in Google Maps</a>
     </div>
 );
